c4c6ee5df56ee494928ded7fc6038107dbd022d8
Atamosa Antonio Jr <atamosaantoniojr@Atamosas-iMac.local>
2014-06-16 11:10:20 +0800

    Initial Commit

---
+++ ZenoRadioApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZenoRadioApp
+//
+//  Created by Atamosa Antonio Jr. on 6/16/14.
+//  Copyright (c) 2014 ICONS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZRAppDelegate class]));
+    }
+}
---
+++ ZenoRadioAppTests/ZenoRadioAppTests.m
@@ -0,0 +1,34 @@
+//
+//  ZenoRadioAppTests.m
+//  ZenoRadioAppTests
+//
+//  Created by Atamosa Antonio Jr. on 6/16/14.
+//  Copyright (c) 2014 ICONS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZenoRadioAppTests : XCTestCase
+
+@end
+
+@implementation ZenoRadioAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
